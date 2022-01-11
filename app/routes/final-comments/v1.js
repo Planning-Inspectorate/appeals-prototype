d38a21db2b1bbcd694877f4b05221217cc0a9150
Westley Knight <westley.knight@gmail.com>
2022-01-11 13:15:42 +0000

    AS-4250 Final Comments submission form

--- app/routes/final-comments/v1.js
+++ app/routes/final-comments/v1.js
@@ -3,17 +3,38 @@ module.exports = function (router) {
   var v = "v1";
   var base = "/final-comments/"+v+"/";
 
-  
-  router.post(base+'appellant', function (req, res) {
-    res.redirect(base+'appellant/check-your-answers');
-  })
-/*
-  router.post(base+'validate-appeal', function (req, res) {
-    res.redirect(base+'review-complete');
-  })
 
-  router.post(base+'what-next', function (req, res) {
-    res.redirect(base+'review-complete');
-  })*/
+  // Appellant routes
+  
+    router.post(base+'appellant', function (req, res) {
+      res.redirect(base+'appellant/check-your-answers');
+    })
+    
+    router.post(base+'appellant/check-your-answers', function (req, res) {
+      res.redirect(base+'appellant/confirmation');
+    })
+
+
+  // Agent routes
+  
+    router.post(base+'agent', function (req, res) {
+      res.redirect(base+'agent/check-your-answers');
+    })
+    
+    router.post(base+'agent/check-your-answers', function (req, res) {
+      res.redirect(base+'agent/confirmation');
+    })
+
+
+  // LPD routes
+  
+    router.post(base+'lpd', function (req, res) {
+      res.redirect(base+'lpd/check-your-answers');
+    })
+    
+    router.post(base+'lpd/check-your-answers', function (req, res) {
+      res.redirect(base+'lpd/confirmation');
+    })
+    
 
 }
