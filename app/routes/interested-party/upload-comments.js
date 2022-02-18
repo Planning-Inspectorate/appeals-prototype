9c2fea582585a14ae9ca78a303d1bc8be8fbecb0
Steve Murphy <murfious@gmail.com>
2022-02-18 17:23:53 +0000

    Fixes and interested party v4

--- app/routes/interested-party/upload-comments.js
+++ app/routes/interested-party/upload-comments.js
@@ -1,6 +1,8 @@
+// version 3 routes
+
 module.exports = function (router) {
 
-  var base = "/interested-party-comments/v3/"
+  var base = "/interested-party/v3/"
 
   router.get(base+'upload-comments-redirect', function (req, res) {
     // take the user to the right screen based on previous actions (if any)
@@ -11,6 +13,56 @@ module.exports = function (router) {
       // show files that have been uploaded
       res.redirect(base+'comment-upload-list');
     }
+  })
+
+  router.post(base+'comment-upload-check', function (req, res) {
+
+    // if array doesn't exist, create it
+    if (!req.session.data['comments']) {
+      req.session.data['comments'] = []
+    }
+
+    // Append uploaded files to the existing array of uploads
+    req.session.data['comments'] = req.session.data['comments'].concat(req.session.data['comments-file']);
+
+    // delete form data so revisiting the upload page doesn't show stored info from last upload
+    req.session.data['comments-file'] = null
+
+    // redirect to list of uploaded files
+    res.redirect(base+'comment-upload-list');
+
+  })
+
+  router.post(base+'comment-delete', function (req, res) {
+
+    // remove item from array
+    req.session.data['comments'].splice(req.session.data['deleterow'],1);
+
+    // redirect the user to the relevant page
+    res.redirect(base+'upload-comments-redirect');
+
+  })
+
+}
+
+
+
+// version 4 routes
+module.exports = function (router) {
+
+  var base = "/interested-party/v4/"
+  router.post(base+'appealOption', function (req, res) {
+
+    // Make a variable from session data
+    let appealType = req.session.data['hasAppealNumber']
+
+    // route depending on value
+    if (appealType === 'appealNumber') {
+      res.redirect('appeal-search-reference')
+    } else {
+      res.redirect('appeal-search-address')
+    }
+
   })
 
   router.post(base+'comment-upload-check', function (req, res) {
