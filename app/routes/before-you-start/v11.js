7e95c69ebd12648fd1b1ec774828986d116d9303
Steve Murphy <murfious@gmail.com>
2022-05-23 23:17:05 +0100

    More enforcement screens

--- app/routes/before-you-start/v11.js
+++ app/routes/before-you-start/v11.js
@@ -154,6 +154,70 @@ module.exports = function (router) {
      }
    })
 
+   // ENF 00X
+   // Enforcement
+   // Site visibility
+   router.post('/sitevisible', function (req, res) {
+
+     // Make a variable from session data
+     let visibility = req.session.data['sitevisibility']
+
+     // route depending on value
+     if (visibility === 'yes') {
+       res.redirect('morepeople')
+     } else {
+       res.redirect('/enforcement/v1/appeal-site/postcode-address')
+     }
+   })
+
+   // ENF 00X
+   // Enforcement
+   // Site interest
+   router.post('/siteinterest', function (req, res) {
+
+     // Make a variable from session data
+     let interest = req.session.data['interest']
+
+     // route depending on value
+     if (interest === 'none') {
+       res.redirect('moreinfo')
+     } else {
+       res.redirect('/enforcement/v1/grounds/grounds')
+     }
+   })
+
+   // ENF 00X
+   // Enforcement
+   // Grounds
+   router.post('/grounds', function (req, res) {
+
+     // Make a variable from session data
+     let groundA = req.session.data['grounds-1']
+     let groundB = req.session.data['grounds-2']
+     let groundC = req.session.data['grounds-3']
+     let groundD = req.session.data['grounds-4']
+     let groundE = req.session.data['grounds-5']
+     let groundF = req.session.data['grounds-6']
+     let groundG = req.session.data['grounds-7']
+
+     // route depending on value
+     if (groundA) {
+       res.redirect('/enforcement/v1/grounds/groundA-text')
+     } else if (groundB) {
+       res.redirect('/enforcement/v1/grounds/groundB-text')
+     } else if (groundC) {
+       res.redirect('/enforcement/v1/grounds/groundC-text')
+     } else if (groundD) {
+       res.redirect('/enforcement/v1/grounds/groundD-text')
+     } else if (groundE) {
+       res.redirect('/enforcement/v1/grounds/groundE-text')
+     } else if (groundF) {
+       res.redirect('/enforcement/v1/grounds/groundF-text')
+     } else {
+       res.redirect('/enforcement/v1/grounds/groundG-text')
+     }
+   })
+
 
  /**********************************
   *** BEFORE YOU START CONTINUES ***
