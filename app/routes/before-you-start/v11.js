303a03828bf01dccc0b3a4e5d648487ae0c9bc14
Steve Murphy <murfious@gmail.com>
2022-05-24 00:33:18 +0100

    Enforcement additions

--- app/routes/before-you-start/v11.js
+++ app/routes/before-you-start/v11.js
@@ -118,110 +118,12 @@ module.exports = function (router) {
    })
 
 
-  /******************************
-   *** ENFORCEMENT SUBMISSION ***
-   ******************************/
-
-   // ENF 00X
-   // Enforcement
-   // Who are you?
-   router.post('/whoareyou', function (req, res) {
-
-     // Make a variable from session data
-     let user = req.session.data['identity']
-
-     // route depending on value
-     if (user === 'appellant') {
-       res.redirect('/enforcement/v1/contact-details/contact-details')
-     } else {
-       res.redirect('/enforcement/v1/agent-enforcement')
-     }
-   })
-
-   // ENF 00X
-   // Enforcement
-   // Additional people
-   router.post('/additionalpeople', function (req, res) {
-
-     // Make a variable from session data
-     let addedpeople = req.session.data['addedpeople']
-
-     // route depending on value
-     if (addedpeople === 'yes') {
-       res.redirect('morepeople')
-     } else {
-       res.redirect('/enforcement/v1/appeal-site/postcode-address')
-     }
-   })
-
-   // ENF 00X
-   // Enforcement
-   // Site visibility
-   router.post('/sitevisible', function (req, res) {
-
-     // Make a variable from session data
-     let visibility = req.session.data['sitevisibility']
-
-     // route depending on value
-     if (visibility === 'yes') {
-       res.redirect('morepeople')
-     } else {
-       res.redirect('/enforcement/v1/appeal-site/postcode-address')
-     }
-   })
-
-   // ENF 00X
-   // Enforcement
-   // Site interest
-   router.post('/siteinterest', function (req, res) {
-
-     // Make a variable from session data
-     let interest = req.session.data['interest']
-
-     // route depending on value
-     if (interest === 'none') {
-       res.redirect('moreinfo')
-     } else {
-       res.redirect('/enforcement/v1/grounds/grounds')
-     }
-   })
-
-   // ENF 00X
-   // Enforcement
-   // Grounds
-   router.post('/grounds', function (req, res) {
-
-     // Make a variable from session data
-     let groundA = req.session.data['grounds-1']
-     let groundB = req.session.data['grounds-2']
-     let groundC = req.session.data['grounds-3']
-     let groundD = req.session.data['grounds-4']
-     let groundE = req.session.data['grounds-5']
-     let groundF = req.session.data['grounds-6']
-     let groundG = req.session.data['grounds-7']
-
-     // route depending on value
-     if (groundA) {
-       res.redirect('/enforcement/v1/grounds/groundA-text')
-     } else if (groundB) {
-       res.redirect('/enforcement/v1/grounds/groundB-text')
-     } else if (groundC) {
-       res.redirect('/enforcement/v1/grounds/groundC-text')
-     } else if (groundD) {
-       res.redirect('/enforcement/v1/grounds/groundD-text')
-     } else if (groundE) {
-       res.redirect('/enforcement/v1/grounds/groundE-text')
-     } else if (groundF) {
-       res.redirect('/enforcement/v1/grounds/groundF-text')
-     } else {
-       res.redirect('/enforcement/v1/grounds/groundG-text')
-     }
-   })
 
 
- /**********************************
-  *** BEFORE YOU START CONTINUES ***
-  **********************************/
+ /***************************************
+  *** BEFORE YOU START CONTINUES ********
+  *** PREVIOUS BEFORE YOU START WORK ****
+  ***************************************/
 
   // Question 2
   router.post(base+'planning-department', function (req, res) {
