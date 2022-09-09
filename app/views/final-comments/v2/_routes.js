2664257bfa722b783042b5193e59b61c60488aa8
Steve Murphy <murfious@gmail.com>
2022-09-09 13:02:11 +0100

    Final comment updates

--- app/views/final-comments/v2/_routes.js
+++ app/views/final-comments/v2/_routes.js
@@ -23,17 +23,33 @@ router.post('/supporting-check', function (req, res) {
 
   // Make a variable from session data
   let documents = req.session.data['supporting-check']
+  let comments = req.session.data['final-comments']
 
   if (documents == 'Yes') {
     res.redirect('supporting-upload')
   } else {
-    res.redirect('confirmation')
+    if (comments != true) {
+      res.redirect('no-final-comments')
+    } else {
+      res.redirect('confirmation')
+    }
   }
 })
 
 // Upload
 //////////////////////
 router.post('/supporting-upload', function (req, res) {
+  let uploaded = req.session.data['supporting-docs']
+  if (uploaded == null) {
+    res.redirect('no-final-comments')
+  } else {
+    res.redirect('confirmation')
+  }
+})
+
+// If nothing has been added, show this
+//////////////////////
+router.post('/no-final-comments', function (req, res) {
   res.redirect('confirmation')
 })
 
