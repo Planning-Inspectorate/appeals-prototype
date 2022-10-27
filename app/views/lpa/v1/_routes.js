8f505fb44df798e91d53f3e011c5fb71a9027230
Steve Murphy <murfious@gmail.com>
2022-10-27 09:55:33 +0100

    Not working: adding users to a list

--- app/views/lpa/v1/_routes.js
+++ app/views/lpa/v1/_routes.js
@@ -31,15 +31,57 @@ router.post('/give-access', function (req, res) {
   res.redirect('check-email')
 })
 
-// check email
+
+// when the email is ok, add a new user
 router.post('/check-email', function (req, res) {
-  res.redirect('access?action=added&shared=yes')
+
+  // make variables from the session data
+  let users = req.session.data['users']
+  let allUsers = req.session.data['allUsers']
+  let email = req.session.data['email']
+
+  // make an object to hold the data
+  let lastUserData = {
+    email
+  };
+
+  // add the data to an object
+  allUsers.push(lastUserData);
+
+  // increment licence numbers
+  if (users === 1) {
+    req.session.data.users = 2;
+  } else if (users === 2) {
+    req.session.data.users = 3;
+  } else if (users === 3) {
+    req.session.data.users = 4;
+  } else if (users > 3) {
+    req.session.data.users = 5; // hardcoded max of 5
+  } else {
+    req.session.data.users = 1;
+  }
+
+  // loop back to the list
+  res.redirect('access?action=added')
+
 })
+
 
 // confirm remove
 router.post('/confirm-remove', function (req, res) {
   res.redirect('access?action=removed&shared=')
 })
+
+
+
+
+
+
+
+
+
+
+
 
 // Add your routes above the module.exports line
 module.exports = router
