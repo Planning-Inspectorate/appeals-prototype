095893950dc43f271ddcee68f2cdc675b8b09055
Chris Armstrong <mail@chrisarmstrong.io>
2022-11-08 16:07:58 +0000

    Added manage users for LPAs

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -2,6 +2,12 @@ let lpas = require('./lpas.json')
 
 module.exports = {
   lpas,
-  "username": "Jim Hanson",
-  "dashboard-lpa": "Cambridgeshire and Peterborough Combined Authority"
+  'username': 'Jim Hanson',
+  'dashboard-lpa': 'Cambridgeshire and Peterborough Combined Authority',
+  'lpa-email': '@cambridgeshirepeterborough-ca.gov.uk',
+  'users': [
+    {
+      'email': 'appeals@cambridgeshirepeterborough-ca.gov.uk'
+    }
+  ]
 }
