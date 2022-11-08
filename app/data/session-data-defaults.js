7974ca5ef58a9242668b5da7e5c2760d0c39f0ad
Chris Armstrong <mail@chrisarmstrong.io>
2022-11-08 09:41:40 +0000

    Changed dashboard default LPA to the longest one

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -3,5 +3,5 @@ let lpas = require('./lpas.json')
 module.exports = {
   lpas,
   "username": "Jim Hanson",
-  "dashboard-lpa": "Leeds City Council"
+  "dashboard-lpa": "Cambridgeshire and Peterborough Combined Authority"
 }
