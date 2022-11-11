f0464dfc41ebb02cf6c4d10b5256c6040af85390
Steve Murphy <murfious@gmail.com>
2022-11-11 23:06:03 +0000

    LPA account updates - emails all linked up now

---
+++ app/views/manage/access/v1/_routes.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const router = new express.Router()
+
+router.get('*', function(req, res, next){
+  // Change the service name for this feature
+  res.locals['serviceName'] = 'Manage your planning inspectorate appeals'
+
+  next()
+})
+
+
+router.post('/enter-code', function (req, res) {
+
+  // find out where the journey started
+  let start = req.session.data['start']
+
+  // route depending on value
+  if (start === 'invite') {
+    res.redirect('/manage/appeals/v2/?no-appeals=true')
+  } else if (start === 'questionnaire') {
+    res.redirect('/manage/questionnaire/v11/task-list')
+  } else {
+    res.redirect('/manage/final-comments/v1/add-comments')
+  }
+
+})
+
+
+// Add your routes above the module.exports line
+module.exports = router
