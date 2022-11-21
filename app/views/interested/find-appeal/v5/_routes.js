260d584148115ec85f63cb654f80b0f3cdaf276d
Steve Murphy <murfious@gmail.com>
2022-11-21 18:39:08 +0000

    Interested party update, v6 for comment on appeal

---
+++ app/views/interested/find-appeal/v5/_routes.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = new express.Router()
+
+router.get('*', function(req, res, next){
+  // Change the service name for this feature
+  res.locals['serviceName'] = 'Find and comment on planning appeals'
+  next()
+})
+
+router.post('/guidance', function (req, res) {
+  res.redirect('find-appeal')
+})
+
+router.post('/find-appeal', function (req, res) {
+  if (req.session.data['appeal-number'] == 'appealNumber') {
+    res.redirect('appeal-search-number')
+  } else {
+    res.redirect('appeal-search-postcode')
+  }
+})
+
+router.post('/documents-upload', function (req, res) {
+  res.redirect('check-your-answers')
+})
+
+// Add your routes above the module.exports line
+module.exports = router
