9509e759e7be238ce21294a79414d44e7c24c8ad
Steve Murphy <steve.murphy@planninginspectorate.gov.uk>
2023-11-01 13:30:16 +0000

    Appeals v7, questionnaire drops down the page

---
+++ app/views/manage/appeals/v7/_routes.js
@@ -0,0 +1,12 @@
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+router.get('*', function(req, res, next){
+  // Change the service name for this feature
+  res.locals['serviceName'] = 'Manage my appeals'
+
+  next()
+})
+
+// Add your routes above the module.exports line
+module.exports = router
