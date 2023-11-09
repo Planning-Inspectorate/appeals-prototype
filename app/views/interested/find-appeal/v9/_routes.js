641e073398f98908580489959c1f3bb16a7425f1
Steve Murphy <steve.murphy@planninginspectorate.gov.uk>
2023-11-09 15:07:42 +0000

    Interested parties content update + decided appeals update

---
+++ app/views/interested/find-appeal/v9/_routes.js
@@ -0,0 +1,31 @@
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+router.get('*', function(req, res, next){
+  // Change the service name for this feature
+  res.locals['serviceName'] = 'Comment on a planning appeal'
+  next()
+})
+
+router.post('/guidance', function (req, res) {
+  res.redirect('find-appeal')
+})
+
+router.post('/find-appeal', function (req, res) {
+  if (req.session.data['appeal-number'] == 'appealNumber') {
+    res.redirect('appeal-search-number')
+  } else {
+    res.redirect('appeal-search-address')
+  }
+})
+
+router.post('/appeal-search-number', function (req, res) {
+  res.redirect('appeal-details?status=open')
+})
+
+router.post('/documents-upload', function (req, res) {
+  res.redirect('check-your-answers')
+})
+
+// Add your routes above the module.exports line
+module.exports = router
