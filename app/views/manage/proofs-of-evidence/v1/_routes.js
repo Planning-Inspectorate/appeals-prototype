86b7223f8af94186cd20f7b0202e9107ff46c612
Steve Murphy <steve.murphy@planninginspectorate.gov.uk>
2023-11-14 16:18:27 +0000

    Appeal views with bigger headings and proof of evidence pages

---
+++ app/views/manage/proofs-of-evidence/v1/_routes.js
@@ -0,0 +1,27 @@
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+router.get('*', function(req, res, next){
+  // Change the service name for this feature
+  res.locals['serviceName'] = 'Appeal a planning decision'
+
+  // Change the service name URL so it goes to the beginning
+  res.locals['serviceUrl'] = '/'+req.originalUrl.split('/')[1]+'/'+req.originalUrl.split('/')[2]+'/'
+
+  next()
+})
+
+router.post('/before-you-continue', function (req, res) {
+  res.redirect('witnesses-and-summary');
+})
+
+router.post('/witnesses-and-summary', function (req, res) {
+  res.redirect('upload-evidence');
+})
+
+router.post('/upload-evidence', function (req, res) {
+  res.redirect('check-your-answers');
+})
+
+// Add your routes above the module.exports line
+module.exports = router
