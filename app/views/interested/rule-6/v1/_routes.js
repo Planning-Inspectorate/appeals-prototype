c884348de35e24b52186f3e6e617c7c60f5c7e6d
Steve Murphy <steve.murphy@planninginspectorate.gov.uk>
2023-11-16 17:49:58 +0000

    Rule 6 appeal views

---
+++ app/views/interested/rule-6/v1/_routes.js
@@ -0,0 +1,12 @@
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+router.get('*', function(req, res, next){
+  // Change the service name for this feature
+  res.locals['serviceName'] = 'Appeal a planning decision'
+
+  next()
+})
+
+// Add your routes above the module.exports line
+module.exports = router
