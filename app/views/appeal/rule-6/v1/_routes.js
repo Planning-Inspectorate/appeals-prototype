aff5246dd11c25a912c45d324ccad4309bc1f3e4
Steve Murphy <steve.murphy@planninginspectorate.gov.uk>
2024-01-05 15:23:21 +0000

    Rule 6 revoked options

--- app/views/appeal/rule-6/v1/_routes.js
+++ app/views/appeal/rule-6/v1/_routes.js
@@ -8,5 +8,24 @@ router.get('*', function(req, res, next){
   next()
 })
 
+
+router.post('/email', function (req, res) {
+  res.redirect('enter-code')
+})
+
+router.post('/enter-code', function (req, res) {
+  if (req.session.data['hub']) {
+    res.redirect('your-appeals')
+  } else {
+
+    if (req.session.data['current-appeals'] != 'no') {
+      res.redirect('appeal-details?questionnaire=complete&lpa-statement=&rule6-statement=due&r6-statement=&ip-comments=&proofs=&lpa-proofs=&appellant-proofs=&timings=&other-timings=&appeal-decided=')
+    } else {
+      res.redirect('access-revoked')
+    }
+
+  }
+})
+
 // Add your routes above the module.exports line
 module.exports = router
