f513e856e00138e70a93f06dff1813b44c8012dd
Steve Murphy <steve.murphy@planninginspectorate.gov.uk>
2024-04-09 16:36:34 +0100

    Contact details update

---
+++ app/views/appeal/contact-details/v1/_routes.js
@@ -0,0 +1,52 @@
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+router.get('*', function(req, res, next){
+  // Change the service name for this feature
+  res.locals['serviceName'] = 'Appeal a planning decision'
+
+  // Change the service name URL so it goes to the beginning
+  res.locals['serviceUrl'] = '/'+req.originalUrl.split('/')[1]+'/'+req.originalUrl.split('/')[2]+'/'
+
+  // include link to return to task list
+  res.locals['return'] = true
+
+  next()
+})
+
+
+
+// CYA links post back to the end
+
+router.post('/who-applied', function (req, res) {
+
+  let applicant = req.session.data['who-applied']
+
+  if (applicant == 'You') {
+    res.redirect('appellant-contact-details');
+  } else if (applicant == 'Another individual') {
+    res.redirect('appellant-name');
+  } else {
+    res.redirect('organisation-name');
+  }
+
+})
+
+router.post('/appellant-name', function (req, res) {
+  res.redirect('agent-contact-details');
+})
+
+router.post('/organisation-name', function (req, res) {
+  res.redirect('agent-contact-details');
+})
+
+router.post('/appellant-contact-details', function (req, res) {
+  res.redirect('/');
+})
+
+router.post('/agent-contact-details', function (req, res) {
+  res.redirect('/');
+})
+
+// Add your routes above the module.exports line
+module.exports = router
