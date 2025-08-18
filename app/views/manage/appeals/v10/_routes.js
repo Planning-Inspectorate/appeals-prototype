815d085bdfc46e13dae6e5f921dd7b37ed50c83b
Steve Murphy <murfious@gmail.com>
2025-08-18 14:00:50 +0100

    v10 of LPA appeals list, using filters

--- app/views/manage/appeals/v10/_routes.js
+++ app/views/manage/appeals/v10/_routes.js
@@ -8,5 +8,18 @@ router.get('*', function(req, res, next){
   next()
 })
 
+// do you want to upload docs?
+router.post('/index', function (req, res) {
+  if (req.session.data['appeals-list'] == 'Active') {
+    res.redirect('/?appeals-list=Active')
+  } else if (req.session.data['appeals-list'] == 'Waiting') {
+    res.redirect('/?appeals-list=Waiting')
+  } else if (req.session.data['appeals-list'] == 'Withdrawn') {
+    res.redirect('/?appeals-list=Withdrawn')
+  } else {
+    res.redirect('/?appeals-list=Decided')
+  }
+})
+
 // Add your routes above the module.exports line
 module.exports = router
