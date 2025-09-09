41ddfef445ed79e55f674cac2817dbb3de3638c6
Steve Murphy <murfious@gmail.com>
2025-09-09 17:06:18 +0100

    LPA can add a venue address

--- app/views/manage/appeals/v9/_routes.js
+++ app/views/manage/appeals/v9/_routes.js
@@ -4,8 +4,15 @@ const router = govukPrototypeKit.requests.setupRouter()
 router.get('*', function(req, res, next){
   // Change the service name for this feature
   res.locals['serviceName'] = 'Manage your appeals'
-
   next()
+})
+
+router.post('/venue-address', function (req, res) {
+  res.redirect('check-venue-address')
+})
+
+router.post('/check-venue-address', function (req, res) {
+  res.redirect('venue-address-confirmation')
 })
 
 // Add your routes above the module.exports line
