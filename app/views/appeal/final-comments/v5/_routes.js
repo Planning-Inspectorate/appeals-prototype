f6f3682258c18ff85527b5fcda8c1e313b5001a4
oliver-rose28 <oliver.rose@planninginspectorate.gov.uk>
2025-10-01 13:47:21 +0100

    Submit final comments

---
+++ app/views/appeal/final-comments/v5/_routes.js
@@ -0,0 +1,46 @@
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+router.get('*', function(req, res, next){
+  // Change the service name for this feature
+  res.locals['serviceName'] = 'Appeal a planning decision'
+
+  // Add return to task list
+  res.locals['return'] = true
+
+  next()
+})
+
+router.post('/final-comment-check', function (req, res) {
+  if (req.session.data['adding-comment'] == 'No') {
+    res.redirect('check-your-answers')
+  } else {
+    res.redirect('submit-final-comments')
+  }
+})
+
+router.post('/submit-final-comments', function (req, res) {
+    res.redirect('final-comments')
+})
+
+router.post('/final-comments', function (req, res) {
+  res.redirect('documents-check')
+})
+
+router.post('/documents-check', function (req, res) {
+  if (req.session.data['adding-documents'] == 'No') {
+    res.redirect('check-your-answers')
+  } else {
+    res.redirect('documents-upload')
+  }
+})
+
+router.post('/documents-upload', function (req, res) {
+  res.redirect('check-your-answers')
+})
+
+
+
+
+// Add your routes above the module.exports line
+module.exports = router
