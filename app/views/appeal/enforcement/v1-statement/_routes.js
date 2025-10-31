e2123f0055a0c5da8258af1f21c1d84f86bc33c8
Claire <claire.mcshane@solirius.com>
2025-10-31 15:48:46 +0000

    appellant statement

---
+++ app/views/appeal/enforcement/v1-statement/_routes.js
@@ -0,0 +1,35 @@
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+router.get('*', function(req, res, next){
+  // Change the service name for this feature
+  res.locals['serviceName'] = 'Manage your planning inspectorate appeals'
+  next()
+})
+// enter code
+router.post('/enter-code', function (req, res) {
+  res.redirect('enter-statement')
+})
+
+// enter statement
+router.post('/enter-statement', function (req, res) {
+  res.redirect('documents-check')
+})
+
+// do you want to upload docs?
+router.post('/documents-check', function (req, res) {
+  if (req.session.data['adding-documents'] == 'Yes') {
+    res.redirect('documents-upload')
+  } else {
+    res.redirect('check-your-answers')
+  }
+})
+
+// upload docs page
+router.post('/documents-upload', function (req, res) {
+  res.redirect('check-your-answers')
+})
+
+
+
+// Add your routes above the module.exports line
+module.exports = router
