1f3919bd4a34ff46be6bdb32bf73cb7bf8e87c79
Steve Murphy <murfious@gmail.com>
2026-01-20 17:25:13 +0000

    Updated header style + access control draft

---
+++ app/views/appeal/change-main-contact/v1/_routes.js
@@ -0,0 +1,19 @@
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+router.get('*', function(req, res, next){
+  // Change the service name for this feature
+  res.locals['serviceName'] = 'Appeal a planning decision'
+
+  // Change the service name URL so it goes to the beginning
+  res.locals['serviceUrl'] = '/'+req.originalUrl.split('/')[1]+'/'+req.originalUrl.split('/')[2]+'/'
+
+  next()
+})
+
+router.post('/email-address', function (req, res) {
+  res.redirect('confirm-change-access');
+})
+
+// Add your routes above the module.exports line
+module.exports = router
